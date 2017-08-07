4ade76a8eea537f76ef4cf71e6f5ed3238d96c07
BIT <BIT@DESKTOP-DOGVD1T>
2017-08-07 17:21:05 +0900

    dao - getList finish

--- src/main/java/com/guestbook/repository/GuestBookDao.java
+++ src/main/java/com/guestbook/repository/GuestBookDao.java
@@ -8,12 +8,17 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.guestbook.vo.GuestBookVo;
 
 @Repository
 public class GuestBookDao {
+	@Autowired
+	private SqlSession sqlSession;
+	
 	public int delete(int no, String password) {
 		// 0. import java.sql.*;
 		Connection conn = null;
@@ -118,58 +123,8 @@ public class GuestBookDao {
 	}
 
 	public List<GuestBookVo> getlist() {
-		// 0. import java.sql.*;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		List<GuestBookVo> list = new ArrayList<GuestBookVo>();
-
-		try {
-
-			// 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-
-			// 2. Connection 얻어오기
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-
-			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = "select no, name, password, content, reg_date " + "from guestbook" + " order by no desc";
-			pstmt = conn.prepareStatement(query);
-
-			rs = pstmt.executeQuery();
-
-			// 4.결과처리
-			while (rs.next()) {
-				int no = rs.getInt("no");
-				String name = rs.getString("name");
-				String password = rs.getString("password");
-				String content = rs.getString("content");
-				String regDate = rs.getString("reg_date");
-
-				list.add(new GuestBookVo(no, name, password, content, regDate));
-			}
-
-		} catch (ClassNotFoundException e) {
-			System.out.println("error: 드라이버 로딩 실패 - " + e);
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			// 5. 자원정리
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-			}
-		}
-
-		return list;
+		List<GuestBookVo> list = sqlSession.selectList("guestbook.getList");
+		return list;	
 	}
 
 }

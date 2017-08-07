3ef42cab476e5ce8ec162f17d71b25a5e8ffe9a3
BIT <BIT@DESKTOP-DOGVD1T>
2017-08-07 17:41:26 +0900

    dao - delete finish

--- src/main/java/com/guestbook/repository/GuestBookDao.java
+++ src/main/java/com/guestbook/repository/GuestBookDao.java
@@ -1,12 +1,8 @@
 package com.guestbook.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,54 +16,11 @@ public class GuestBookDao {
 	private SqlSession sqlSession;
 	
 	public int delete(int no, String password) {
-		// 0. import java.sql.*;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		int count = -1;
-
-		try {
-
-			// 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-
-			// 2. Connection 얻어오기
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-
-			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = "delete from guestbook "
-					+ "where no=? and password=?";
-			pstmt = conn.prepareStatement(query);
-
-			pstmt.setInt(1, no);
-			pstmt.setString(2, password);
-
-			count = pstmt.executeUpdate();
-
-			// 4.결과처리
-
-			System.out.println(count + "건 처리");
-
-		} catch (ClassNotFoundException e) {
-			System.out.println("error: 드라이버 로딩 실패 - " + e);
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			// 5. 자원정리
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-			}
-		}
-
-		return count;
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("no", no);
+		map.put("password", password);
+		
+		return sqlSession.delete("guestbook.delete", map);
 	}
 
 	public int insert(GuestBookVo vo) {
